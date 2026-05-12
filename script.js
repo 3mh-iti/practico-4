77a18fe23c83cddd7c41147f3d0fab0ac785cc36
nDaCunha <ndac08@gmail.com>
2026-05-12 10:57:29 -0300

    Avance en JS, completar parte 3

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+const formulario = document.querySelector("#formulario-producto");
+const nombre = document.querySelector("#nombre")
+const precio = document.querySelector("#precio")
+
+let datos = {
+    nombre: nombre.input,
+    precio: precio.input
+}
+
 let inventario = [
     { nombre: "Guitarra Clásica", precio: 15000 },
     { nombre: "Amplificador 15W", precio: 8500 },
-    { nombre: "Set de Cuerdas", precio: 800 }
+    { nombre: "Set de Cuerdas", precio: 800 } 
 ];
+
+formulario.addEventListener('submit', function submit(){
+    event.preventDefault;
+    inventario.push(datos);
+    renderizarInventario();
+});
 
 function renderizarInventario() {
     let contenedor = document.getElementById("listado");
@@ -11,8 +26,8 @@ function renderizarInventario() {
     for (let i = 0; i < inventario.length; i++) {
         let articulo = inventario[i];
         htmlGenerado += "<article class='tarjeta-producto' data-indice='" + i + "'>";
-        htmlGenerado += "<article class='tarjeta-nombre'>" + articulo.nombre + "</div>";
-        htmlGenerado += "<article class='tarjeta-precio'>$" + articulo.precio + "</div>";
+        htmlGenerado += "<article class='tarjeta-nombre'>" + articulo.nombre + "</article>";
+        htmlGenerado += "<article class='tarjeta-precio'>$" + articulo.precio + "</article>";
         htmlGenerado += "</article>";
     }
 
