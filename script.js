b1fe62bb89fd0954429e072031ce399175aa2af2
Joaquin Schol <joacoschol@gmail.com>
2026-05-12 10:40:31 -0300

    mas cambios (media query y correccion de erores html

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ function renderizarInventario() {
 
     for (let i = 0; i < inventario.length; i++) {
         let articulo = inventario[i];
-        htmlGenerado += "<div class='tarjeta-producto' data-indice='" + i + "'>";
+        htmlGenerado += "<article class='tarjeta-producto' data-indice='" + i + "'>";
         htmlGenerado += "<div class='tarjeta-nombre'>" + articulo.nombre + "</div>";
         htmlGenerado += "<div class='tarjeta-precio'>$" + articulo.precio + "</div>";
-        htmlGenerado += "</div>";
+        htmlGenerado += "</article>";
     }
 
     contenedor.innerHTML = htmlGenerado;
