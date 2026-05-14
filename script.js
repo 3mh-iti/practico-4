15f2afb580078b5dda87824f3b7797de37d72d54
vBriceño <victorbriceno899@gmail.com>
2026-05-14 11:59:52 -0300

    Se ajustaron unos detalles en el js en la lista de productos, se le agregó un li para mejorar su semántica

--- script.js
+++ script.js
@@ -31,10 +31,12 @@ function renderizarInventario() {
 
     for (let i = 0; i < inventario.length; i++) {
         let articulo = inventario[i];
+        htmlGenerado += "<li>";
         htmlGenerado += "<article class='tarjeta-producto' data-indice='" + i + "'>";
-        htmlGenerado += "<article class='tarjeta-nombre'>" + articulo.nombre + "</article>";
-        htmlGenerado += "<article class='tarjeta-precio'>$" + articulo.precio + "</article>";
+        htmlGenerado += "<p class='tarjeta-nombre'>" + articulo.nombre + "</p>";
+        htmlGenerado += "<p class='tarjeta-precio'>$" + articulo.precio + "</p >";
         htmlGenerado += "</article>";
+        htmlGenerado += "</li>";
     }
 
     contenedor.innerHTML = htmlGenerado;
