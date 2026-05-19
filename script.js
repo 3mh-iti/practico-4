8b7b2a93df2af3f21fea8ff4005f9d9d61e676bf
profemarcoslucia <profemarcoslucia@gmail.com>
2026-05-19 07:25:13 -0300

    Parte 1 del practico: cambios semanticos

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ function renderizarInventario() {
 
     for (let i = 0; i < inventario.length; i++) {
         let articulo = inventario[i];
-        htmlGenerado += "<div class='tarjeta-producto' data-indice='" + i + "'>";
-        htmlGenerado += "<div class='tarjeta-nombre'>" + articulo.nombre + "</div>";
-        htmlGenerado += "<div class='tarjeta-precio'>$" + articulo.precio + "</div>";
-        htmlGenerado += "</div>";
+        htmlGenerado += "<li class='tarjeta-producto' data-indice='" + i + "'>";
+        htmlGenerado += "<p class='tarjeta-nombre'>" + articulo.nombre + "</p>";
+        htmlGenerado += "<p class='tarjeta-precio'>$" + articulo.precio + "</p>";
+        htmlGenerado += "</li>";
     }
 
     contenedor.innerHTML = htmlGenerado;
