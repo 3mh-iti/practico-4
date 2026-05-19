98e5c3d7363a69c9867aa6dd1c8b7243ace8f66f
Santiagoy <767pela@gmail.com>
2026-05-19 08:45:44 -0300

    Hecho el merge

--- script.js
+++ script.js
@@ -27,35 +27,18 @@ function renderizarInventario() {
 
     for (let i = 0; i < inventario.length; i++) {
         let articulo = inventario[i];
-        htmlGenerado += "<article class='tarjeta-producto' data-indice='" + i + "'>";
-        htmlGenerado += "<article class='tarjeta-nombre'>" + articulo.nombre + "</article>";
-        htmlGenerado += "<article class='tarjeta-precio'>$" + articulo.precio + "</article>";
-        htmlGenerado += "</article>";
+        htmlGenerado += "<li class='tarjeta-producto' data-indice='" + i + "'>";
+        htmlGenerado += "<p class='tarjeta-nombre'>" + articulo.nombre + "</p>";
+        htmlGenerado += "<p class='tarjeta-precio'>$" + articulo.precio + "</p>";
+        htmlGenerado += "</li>";
     }
     contenedor.innerHTML = htmlGenerado;
-}
-const formulario = document.getElementById("formulario-producto");
 
-formulario.addEventListener("submit", function(evento) {
-    
-    evento.preventDefault();
-    
-    const nombre = document.getElementById("nombre").value;
-    const precio = document.getElementById("precio").value;
-    const nuevoProducto = { nombre, precio };
-    
-    inventario.push(nuevoProducto);
-    
-    renderizarInventario();
-    asignarEventosATarjetas();  
-    formulario.reset();
-});
-function clicktarjetas() {
-    const tarjetas = document.querySelectorAll(".tarjeta-producto");
-    
-    for (let i = 0; i < tarjetas.length; i++) {
-        tarjetas[i].addEventListener("click", function() {
-            tarjetas[i].classList.toggle("producto-seleccionado");
+    const productos = document.querySelectorAll(".tarjeta-producto");
+    for (let i = 0; i < productos.length; i++) {
+        const producto = productos[i];
+        producto.addEventListener("click", function (e) {
+            producto.classList.toggle("producto-seleccionado");
         });
     }
 }
@@ -63,4 +46,23 @@ function clicktarjetas() {
 // Carga inicial
 renderizarInventario();
 
-clicktarjetas();
+formulario.addEventListener("submit", function (event) {
+    event.preventDefault();
+    // Obtener los valores del formulario
+    let nombreInput = document.getElementById("nombre");
+    let precioInput = document.getElementById("precio");
+
+    // Crear un nuevo artículo y agregarlo al inventario
+
+    let nuevoArticulo = {
+        nombre: nombreInput.value,
+        precio: precioInput.value,
+    };
+
+    inventario.push(nuevoArticulo);
+    renderizarInventario();
+
+    // Limpiar los campos del formulario
+    nombreInput.value = "";
+    precioInput.value = "";
+});
