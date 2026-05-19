b165a8626444c48580f44b1a953086d655c9ea6b
Santiagoy <767pela@gmail.com>
2026-05-19 13:44:35 -0300

    forEach hecho

--- script.js
+++ script.js
@@ -25,22 +25,20 @@ function renderizarInventario() {
     let contenedor = document.getElementById("listado");
     let htmlGenerado = "";
 
-    for (let i = 0; i < inventario.length; i++) {
-        let articulo = inventario[i];
+    inventario.forEach(function (articulo, i) {
         htmlGenerado += "<li class='tarjeta-producto' data-indice='" + i + "'>";
         htmlGenerado += "<p class='tarjeta-nombre'>" + articulo.nombre + "</p>";
         htmlGenerado += "<p class='tarjeta-precio'>$" + articulo.precio + "</p>";
         htmlGenerado += "</li>";
-    }
+    });
     contenedor.innerHTML = htmlGenerado;
 
     const productos = document.querySelectorAll(".tarjeta-producto");
-    for (let i = 0; i < productos.length; i++) {
-        const producto = productos[i];
+    Array.from(productos).forEach(function (producto) {
         producto.addEventListener("click", function (e) {
             producto.classList.toggle("producto-seleccionado");
         });
-    }
+    });
 }
 
 // Carga inicial
@@ -62,7 +60,7 @@ formulario.addEventListener("submit", function (event) {
     inventario.push(nuevoArticulo);
     renderizarInventario();
 
-    // Limpiar los campos del formulario
+    // Limpiar los campos del formulario.
     nombreInput.value = "";
     precioInput.value = "";
 });
