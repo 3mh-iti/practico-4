58765fd20ea4fd8aa9e69ddc0db8880f00e2aa17
QiQiLovers <santiagonicolas820@gmail.com>
2026-05-26 09:17:11 -0300

    We add a edit button

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const formulario = document.querySelector("#formulario-producto");
 const buscador = document.querySelector("#buscador");
 
 let inventario = [
-  { nombre: "Guitarra Clásica", precio: 15000 },
-  { nombre: "Amplificador 15W", precio: 8500 },
-  { nombre: "Set de Cuerdas", precio: 800 },
+  { nombre: "Guitarra Clásica", precio: 15000, id: 1 },
+  { nombre: "Amplificador 15W", precio: 8500, id: 2 },
+  { nombre: "Set de Cuerdas", precio: 800, id: 3 },
 ];
+
+let idEditando = null;
 
 function renderizarInventario(listaProductos = inventario) {
   let contenedor = document.querySelector("#listado");
@@ -23,8 +25,15 @@ function renderizarInventario(listaProductos = inventario) {
     artPrecio.classList.add("tarjeta-precio");
     artPrecio.textContent = "$" + articulo.precio;
 
+    let BtnEditar = document.createElement("button");
+    BtnEditar.classList.add("tarjeta-btn-editar");
+    BtnEditar.textContent = "Editar";
+
+    BtnEditar.dataset.id = articulo.id;
+
     section.appendChild(artNombre);
     section.appendChild(artPrecio);
+    section.appendChild(BtnEditar);
     contenedor.appendChild(section);
   });
 }
@@ -37,12 +46,23 @@ formulario.addEventListener("submit", function (event) {
   let nombreInput = document.getElementById("nombre");
   let precioInput = document.getElementById("precio");
 
-  let nuevoArticulo = {
-    nombre: nombreInput.value,
-    precio: Number(precioInput.value),
-  };
+  if (idEditando !== null) {
+    let articulo = inventario.find((articulo) => articulo.id == idEditando);
+    if (articulo) {
+      articulo.nombre = nombreInput.value;
+      articulo.precio = Number(precioInput.value);
+    }
+    idEditando = null;
+  } else {
+    let nuevoId = inventario.lastIndexOf(inventario[inventario.length - 1]) + 2;
+    let nuevoArticulo = {
+      nombre: nombreInput.value,
+      precio: Number(precioInput.value),
+      id: nuevoId,
+    };
+    inventario.push(nuevoArticulo);
+  }
 
-  inventario.push(nuevoArticulo);
   renderizarInventario();
 
   nombreInput.value = "";
@@ -54,6 +74,7 @@ buscador.addEventListener("keyup", function (e) {
 
   if (textoBuscado === "") {
     renderizarInventario();
+    2;
     return;
   }
 
@@ -66,6 +87,17 @@ buscador.addEventListener("keyup", function (e) {
 
 document.getElementById("listado").addEventListener("click", function (event) {
   event.preventDefault();
+  if (event.target.classList.contains("tarjeta-btn-editar")) {
+    let id = event.target.dataset.id;
+    const encontrado = inventario.find((articulo) => articulo.id == id);
+
+    formulario.nombre.value = encontrado.nombre;
+    formulario.precio.value = encontrado.precio;
+
+    idEditando = encontrado.id;
+
+    return;
+  }
 
   let tarjeta = event.target.closest(".tarjeta-producto");
   if (tarjeta) {
