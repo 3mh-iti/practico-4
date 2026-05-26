e1b91148f577726d976ef4ee6511ffc6e5c56404
profemrluciaveit <profemarcoslucia@gmail.com>
2026-05-26 10:48:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
 let inventario = [
-    { nombre: "Guitarra Clásica", precio: 15000 },
-    { nombre: "Amplificador 15W", precio: 8500 },
-    { nombre: "Set de Cuerdas", precio: 800 },
+    { id: crypto.randomUUID(), nombre: "Guitarra Clásica", precio: 15000 },
+    { id: crypto.randomUUID(), nombre: "Amplificador 15W", precio: 8500 },
+    { id: crypto.randomUUID(), nombre: "Set de Cuerdas", precio: 800 },
 ];
 
 const formulario = document.querySelector("form");
 const inptNombre = document.querySelector("#nombre");
 const inptPrecio = document.querySelector("#precio");
+const inptId = document.querySelector("#id");
 
 formulario.addEventListener("submit", function (e) {
     e.preventDefault();
     const nombre = inptNombre.value;
     const precio = inptPrecio.value;
+    const id = inptId.value;
+    if(id){
+      inventario = inventario.map(function(producto){
+        if(producto.id === id)
+          return {
+            nombre: nombre, precio: precio, id: id
+          }
+        return producto;
+      });
+      inptId.value = "";
+      formulario.reset();
+      renderizarInventario();
+      formulario.querySelector("button").textContent = "Guardar articulo";
+      return;
+    }
     const producto = {
         nombre,
         precio,
+        id: crypto.randomUUID()
     }
     inventario.push(producto);
     renderizarInventario();
@@ -27,22 +44,35 @@ function renderizarInventario() {
 
     inventario.forEach(function(producto){
       const tarjeta = document.createElement("li");
+      tarjeta.dataset.productId = producto.id;
       tarjeta.classList.add("tarjeta-producto");
       const nombre = document.createElement("p");
       nombre.classList.add("tarjeta-nombre");
       const precio = document.createElement("p");
       precio.classList.add('tarjeta-precio');
+      const modificar = document.createElement("button");
+      modificar.classList.add("tarjeta-button");
 
       nombre.textContent = producto.nombre;
       precio.textContent = "$"+producto.precio;
+      modificar.textContent = "Editar"
 
       tarjeta.appendChild(nombre);
       tarjeta.appendChild(precio);
+      tarjeta.append(modificar)
       contenedor.append(tarjeta);
 
       tarjeta.addEventListener("click", function(e){
         e.preventDefault();
         tarjeta.classList.toggle("producto-seleccionado");
+      });
+
+      modificar.addEventListener("click", function(e){
+        e.preventDefault();
+        inptNombre.value = producto.nombre;
+        inptPrecio.value = producto.precio;
+        inptId.value = producto.id;
+        formulario.querySelector("button").textContent = "Modificar articulo";
       });
     });
 }
