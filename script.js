6e8f121e03c40510b63b3c6ce5015e72cff08127
000f4ku <rodriguezfacu127@gmail.com>
2026-05-30 11:58:41 -0300

    boton editar

--- script.js
+++ script.js
@@ -1,18 +1,98 @@
 let inventario = [
+<<<<<<< Updated upstream
     { nombre: "Guitarra Clásica", precio: 15000 },
     { nombre: "Amplificador 15W", precio: 8500 },
     { nombre: "Set de Cuerdas", precio: 800 }
 ];
 
+=======
+    { id: 1, nombre: "Guitarra Clásica", precio: 15000 },
+    { id: 2, nombre: "Amplificador 15W", precio: 8500 },
+    { id: 3, nombre: "Set de Cuerdas", precio: 800 },
+];
+
+const formulario = document.querySelector("form");
+const inptNombre = document.querySelector("#nombre");
+const inptPrecio = document.querySelector("#precio");
+
+let idEditando = 0;
+
+function generarId() {
+    let nuevoId = 1;
+    inventario.forEach(function (producto) {
+     if (producto.id >= nuevoId) {
+     nuevoId = producto.id + 1;
+        }
+    });
+    return nuevoId;
+}
+
+formulario.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const nombre = inptNombre.value;
+    const precio = inptPrecio.value;
+
+    if (idEditando === 1) {
+        const producto = { id: generarId(), nombre: nombre, precio: precio };
+        inventario.push(producto);
+    } else {
+        inventario.forEach(function (producto) {
+            if (producto.id === idEditando) {
+                producto.nombre = nombre;
+                producto.precio = precio;
+            }
+        });
+        idEditando = 1;
+    }
+
+    renderizarInventario();
+    formulario.reset();
+});
+
+>>>>>>> Stashed changes
 function renderizarInventario() {
     let contenedor = document.getElementById("listado");
     let htmlGenerado = "";
 
+<<<<<<< Updated upstream
     inventario.forEach((articulo, i ) => {
         htmlGenerado += "<article class='tarjeta-producto' data-indice='" + i + "'>";
         htmlGenerado += "<div class='tarjeta-nombre'>" + articulo.nombre + "</div>";
         htmlGenerado += "<div class='tarjeta-precio'>$" + articulo.precio + "</div>";
         htmlGenerado += "</article>";
+=======
+    inventario.forEach(function (producto) {
+        const tarjeta = document.createElement("li");
+        tarjeta.classList.add("tarjeta-producto");
+
+        const nombre = document.createElement("p");
+        nombre.classList.add("tarjeta-nombre");
+
+        const precio = document.createElement("p");
+        precio.classList.add("tarjeta-precio");
+
+        nombre.textContent = producto.nombre;
+        precio.textContent = "$" + producto.precio;
+
+        const btnEditar = document.createElement("button");
+        btnEditar.textContent = "Editar";
+
+        btnEditar.addEventListener("click", function (e) {
+            e.preventDefault
+            idEditando = producto.id;
+            inptNombre.value = producto.nombre;
+            inptPrecio.value = producto.precio;
+        });
+
+        tarjeta.appendChild(nombre);
+        tarjeta.appendChild(precio);
+        tarjeta.appendChild(btnEditar);
+        contenedor.append(tarjeta);
+
+        tarjeta.addEventListener("click", function (e) {
+            e.preventDefault();
+        });
+>>>>>>> Stashed changes
     });
 
 
@@ -47,5 +127,4 @@ formulario.addEventListener("submit", function(evento) {
 
 
 
-// Carga inicial
 renderizarInventario();
